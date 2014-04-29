5b68e48f055ae1a55a3276db5e3f4166f8d3ff94
Carson Gross <carsongross@gmail.com>
2014-04-28 20:42:54 -0700

    Tests and get the transitions API reasonably correct

--- src/intercooler.js
+++ src/intercooler.js
@@ -26,101 +26,97 @@ var Intercooler = Intercooler || (function () {
   //============================================================
   // Base Transition Definitions
   //============================================================
-  var _transitions = {
-    'none': function (elt, kind, content, onCompletion) {
-      if (kind == 'remove') {
-        console.log("deleting...");
-        console.log(elt);
-        elt.remove();
-      } else if (kind == 'hide') {
-        elt.hide();
-      } else if (kind == 'show') {
-        elt.show();
-      } else {
-        elt.html(content);
-      }
-      onCompletion();
-    },
-    'fadeFast': function (elt, kind, content, onCompletion) {
-      if (kind == 'remove') {
-        elt.fadeOut('fast', function () {
-          elt.remove();
-          onCompletion();
-        });
-      } else if (kind == 'hide') {
-        elt.fadeOut('fast', function () {
-          onCompletion();
-        });
-      } else if (kind == 'show') {
-        elt.fadeIn('fast', function () {
-          onCompletion();
-        });
-      } else {
-        elt.fadeOut('fast', function(){
-          elt.html(content);
-          onCompletion();
-          elt.fadeIn('fast');
-        });
-      }
-    },
-    'prepend': function (elt, kind, content, onCompletion) {
-      if(kind != 'update') {
-        throw "'prepend' transition only supports replace"
-      }
-      var elts = $(content);
-      elts.hide();
-      elt.prepend(elts);
-      elts.fadeIn();
-      onCompletion();
-      if (elt.attr('ic-limit-children')) {
-        var limit = parseInt(elt.attr('ic-limit-children'));
-        if (elt.children().length > limit) {
-          elt.children().slice(limit, elt.children().length).remove();
-        }
-      }
-    },
-    'append': function (elt, kind, content, onCompletion) {
-      if(kind != 'update') {
-        throw "'append' transition only supports replace"
-      }
-      var elts = $(content);
-      elts.hide();
-      elt.append(elts);
-      elts.fadeIn();
-      onCompletion();
-      if (elt.attr('ic-limit-children')) {
-        var limit = parseInt(elt.attr('ic-limit-children'));
-        if (elt.children().length > limit) {
-          elt.children().slice(0, elt.children().length - limit).remove();
-        }
-      }
-
-    }
-  };
+  var _transitions = {};
   var _defaultTransition = 'fadeFast';
+  function _defineTransition(name, def) {
+    if(def.newContent == null) {
+      //noinspection JSUnusedLocalSymbols
+      def.newContent = function(parent, newContent, isReverse, after) {
+        parent.html(newContent);
+        after();
+      }
+    }
+    if(def.remove == null) {
+      def.remove = function(elt) {
+        elt.remove();
+      }
+    }
+    if(def.show == null){
+      def.show = function(elt) {
+        elt.show();
+      }
+    }
+    if(def.hide == null){
+      def.show = function(elt) {
+        elt.hide();
+      }
+    }
+    _transitions[name] = def;
+  }
+  _defineTransition('none', {});
+  _defineTransition('fadeFast', {
+    newContent : function(parent, newContent, isReverse, after){
+      parent.fadeOut('fast', function(){
+        parent.html(newContent);
+        after();
+        parent.fadeIn('fast');
+      })
+    },
+    remove : function(elt) {
+      elt.fadeOut('fast', function(){ elt.remove(); })
+    },
+    show : function(elt) {
+      elt.fadeIn('fast');
+    },
+    hide : function(elt) {
+      elt.fadeOut('fast');
+    }
+  });
+  _defineTransition('prepend', {
+    newContent : function(parent, newContent, isReverse, after){
+      var children = $(newContent);
+      children.hide();
+      parent.prepend(children);
+      after();
+      children.fadeIn();
+      if (parent.attr('ic-limit-children')) {
+        var limit = parseInt(parent.attr('ic-limit-children'));
+        if (parent.children().length > limit) {
+          parent.children().slice(limit, parent.children().length).remove();
+        }
+      }
+    }
+  });
+  _defineTransition('append', {
+    newContent : function(parent, newContent, isReverse, after){
+      var children = $(newContent);
+      children.hide();
+      parent.append(children);
+      after();
+      children.fadeIn();
+      if (parent.attr('ic-limit-children')) {
+        var limit = parseInt(parent.attr('ic-limit-children'));
+        if (parent.children().length > limit) {
+          parent.children().slice(0, parent.children().length - limit).remove();
+        }
+      }
+    }
+  });
 
   //============================================================
   // Utility Methods
   //============================================================
 
-  /*
-   CryptoJS v3.1.2
-   code.google.com/p/crypto-js
-   (c) 2009-2013 by Jeff Mott. All rights reserved.
-   code.google.com/p/crypto-js/wiki/License
-   */
-  var CryptoJS=CryptoJS||function(e,m){var p={},j=p.lib={},l=function(){},f=j.Base={extend:function(a){l.prototype=this;var c=new l;a&&c.mixIn(a);c.hasOwnProperty("init")||(c.init=function(){c.$super.init.apply(this,arguments)});c.init.prototype=c;c.$super=this;return c},create:function(){var a=this.extend();a.init.apply(a,arguments);return a},init:function(){},mixIn:function(a){for(var c in a)a.hasOwnProperty(c)&&(this[c]=a[c]);a.hasOwnProperty("toString")&&(this.toString=a.toString)},clone:function(){return this.init.prototype.extend(this)}},
-    n=j.WordArray=f.extend({init:function(a,c){a=this.words=a||[];this.sigBytes=c!=m?c:4*a.length},toString:function(a){return(a||h).stringify(this)},concat:function(a){var c=this.words,q=a.words,d=this.sigBytes;a=a.sigBytes;this.clamp();if(d%4)for(var b=0;b<a;b++)c[d+b>>>2]|=(q[b>>>2]>>>24-8*(b%4)&255)<<24-8*((d+b)%4);else if(65535<q.length)for(b=0;b<a;b+=4)c[d+b>>>2]=q[b>>>2];else c.push.apply(c,q);this.sigBytes+=a;return this},clamp:function(){var a=this.words,c=this.sigBytes;a[c>>>2]&=4294967295<<
-      32-8*(c%4);a.length=e.ceil(c/4)},clone:function(){var a=f.clone.call(this);a.words=this.words.slice(0);return a},random:function(a){for(var c=[],b=0;b<a;b+=4)c.push(4294967296*e.random()|0);return new n.init(c,a)}}),b=p.enc={},h=b.Hex={stringify:function(a){var c=a.words;a=a.sigBytes;for(var b=[],d=0;d<a;d++){var f=c[d>>>2]>>>24-8*(d%4)&255;b.push((f>>>4).toString(16));b.push((f&15).toString(16))}return b.join("")},parse:function(a){for(var c=a.length,b=[],d=0;d<c;d+=2)b[d>>>3]|=parseInt(a.substr(d,
-      2),16)<<24-4*(d%8);return new n.init(b,c/2)}},g=b.Latin1={stringify:function(a){var c=a.words;a=a.sigBytes;for(var b=[],d=0;d<a;d++)b.push(String.fromCharCode(c[d>>>2]>>>24-8*(d%4)&255));return b.join("")},parse:function(a){for(var c=a.length,b=[],d=0;d<c;d++)b[d>>>2]|=(a.charCodeAt(d)&255)<<24-8*(d%4);return new n.init(b,c)}},r=b.Utf8={stringify:function(a){try{return decodeURIComponent(escape(g.stringify(a)))}catch(c){throw Error("Malformed UTF-8 data");}},parse:function(a){return g.parse(unescape(encodeURIComponent(a)))}},
-    k=j.BufferedBlockAlgorithm=f.extend({reset:function(){this._data=new n.init;this._nDataBytes=0},_append:function(a){"string"==typeof a&&(a=r.parse(a));this._data.concat(a);this._nDataBytes+=a.sigBytes},_process:function(a){var c=this._data,b=c.words,d=c.sigBytes,f=this.blockSize,h=d/(4*f),h=a?e.ceil(h):e.max((h|0)-this._minBufferSize,0);a=h*f;d=e.min(4*a,d);if(a){for(var g=0;g<a;g+=f)this._doProcessBlock(b,g);g=b.splice(0,a);c.sigBytes-=d}return new n.init(g,d)},clone:function(){var a=f.clone.call(this);
-      a._data=this._data.clone();return a},_minBufferSize:0});j.Hasher=k.extend({cfg:f.extend(),init:function(a){this.cfg=this.cfg.extend(a);this.reset()},reset:function(){k.reset.call(this);this._doReset()},update:function(a){this._append(a);this._process();return this},finalize:function(a){a&&this._append(a);return this._doFinalize()},blockSize:16,_createHelper:function(a){return function(c,b){return(new a.init(b)).finalize(c)}},_createHmacHelper:function(a){return function(b,f){return(new s.HMAC.init(a,
-    f)).finalize(b)}}});var s=p.algo={};return p}(Math);
-  (function(){var e=CryptoJS,m=e.lib,p=m.WordArray,j=m.Hasher,l=[],m=e.algo.SHA1=j.extend({_doReset:function(){this._hash=new p.init([1732584193,4023233417,2562383102,271733878,3285377520])},_doProcessBlock:function(f,n){for(var b=this._hash.words,h=b[0],g=b[1],e=b[2],k=b[3],j=b[4],a=0;80>a;a++){if(16>a)l[a]=f[n+a]|0;else{var c=l[a-3]^l[a-8]^l[a-14]^l[a-16];l[a]=c<<1|c>>>31}c=(h<<5|h>>>27)+j+l[a];c=20>a?c+((g&e|~g&k)+1518500249):40>a?c+((g^e^k)+1859775393):60>a?c+((g&e|g&k|e&k)-1894007588):c+((g^e^
-    k)-899497514);j=k;k=e;e=g<<30|g>>>2;g=h;h=c}b[0]=b[0]+h|0;b[1]=b[1]+g|0;b[2]=b[2]+e|0;b[3]=b[3]+k|0;b[4]=b[4]+j|0},_doFinalize:function(){var f=this._data,e=f.words,b=8*this._nDataBytes,h=8*f.sigBytes;e[h>>>5]|=128<<24-h%32;e[(h+64>>>9<<4)+14]=Math.floor(b/4294967296);e[(h+64>>>9<<4)+15]=b;f.sigBytes=4*e.length;this._process();return this._hash},clone:function(){var e=j.clone.call(this);e._hash=this._hash.clone();return e}});e.SHA1=j._createHelper(m);e.HmacSHA1=j._createHmacHelper(m)})();
-
-  function fp(elt) {
-    return CryptoJS.SHA1(elt).toString();
+  function fingerprint(elt) {
+    var str = elt.toString();
+    var fp = 0, i, chr, len;
+    if (str.length == 0) return fp;
+    for (i = 0, len = str.length; i < len; i++) {
+      chr = str.charCodeAt(i);
+      fp = ((fp << 5) - fp) + chr;
+      fp |= 0; // Convert to 32bit integer
+    }
+    return fp;
   }
 
   function log(elt, msg, level) {
@@ -172,7 +168,6 @@ var Intercooler = Intercooler || (function () {
   //============================================================
   // Request/Parameter/Include Processing
   //============================================================
-
   function getTarget(elt) {
     if(elt.attr('ic-target') && elt.attr('ic-target').indexOf('this.') != 0) {
       return $(elt.attr('ic-target'));
@@ -184,7 +179,7 @@ var Intercooler = Intercooler || (function () {
   function processHeaders(elt, xhr, pop) {
 
     elt.trigger("beforeHeaders.ic", elt, xhr);
-
+    var target = null;
     if (xhr.getResponseHeader("X-IC-Refresh")) {
       var pathsToRefresh = xhr.getResponseHeader("X-IC-Refresh").split(",");
       log(elt, "IC HEADER: refreshing " + pathsToRefresh, "DEBUG");
@@ -213,15 +208,15 @@ var Intercooler = Intercooler || (function () {
     }
     if(xhr.getResponseHeader("X-IC-Transition")) {
       log(elt, "IC HEADER: setting transition to  " + xhr.getResponseHeader("X-IC-Transition"), "DEBUG");
-      var target = getTarget(elt);
+      target = getTarget(elt);
       target.data("ic-tmp-transition", xhr.getResponseHeader("X-IC-Transition"));
     }
     if (xhr.getResponseHeader("X-IC-Remove")) {
       if (elt) {
-        var target = getTarget(elt);
+        target = getTarget(elt);
         log(elt, "IC REMOVE", "DEBUG");
         var transition = getTransition(elt, target);
-        transition(target, 'remove', null, function(){});
+        transition.remove(target);
       }
     }
 
@@ -447,7 +442,7 @@ var Intercooler = Intercooler || (function () {
   }
 
   function updateIntercoolerMetaData(elt) {
-    elt.attr('ic-fingerprint', fp(elt.text()));
+    elt.attr('ic-fingerprint', fingerprint(elt.html()));
     elt.attr('ic-last-refresh', new Date().getTime());
   }
 
@@ -531,7 +526,7 @@ var Intercooler = Intercooler || (function () {
   //============================================================----
 
   function refreshDependencies(dest, src) {
-    log(src, "Refreshing Dependencies for " + dest, "DEBUG")
+    log(src, "Refreshing Dependencies for " + dest, "DEBUG");
     $('[ic-src]').each(function () {
       var fired = false;
       if(verbFor($(this)) == "GET" && $(this).attr('ic-deps') != 'ignore') {
@@ -592,9 +587,11 @@ var Intercooler = Intercooler || (function () {
         initScrollHandler();
         setTimeout(function () { $(window).trigger('scroll'); }, 100); // Trigger a scroll in case element is already viewable
       } else {
-        $(elt).on(eventFor($(elt).attr('ic-trigger-on'), $(elt)), function () {
+        $(elt).on(eventFor($(elt).attr('ic-trigger-on'), $(elt)), function (e) {
           fireICRequest($(elt));
           refreshDependencies($(elt).attr('ic-src'), $(elt));
+          e.preventDefault();
+          return false;
         });
       }
     }
@@ -648,17 +645,19 @@ var Intercooler = Intercooler || (function () {
       setIfAbsent(elt, 'ic-deps', 'ignore');
     }
     // non-action attributes
+    var value = null;
+    var url = null;
     if(macro == 'ic-style-src') {
-      var value = elt.attr('ic-style-src').split(":");
+      value = elt.attr('ic-style-src').split(":");
       var styleAttribute = value[0];
-      var url = value[1];
+      url = value[1];
       setIfAbsent(elt, 'ic-src', url);
       setIfAbsent(elt, 'ic-target', 'this.style.' + styleAttribute);
     }
     if(macro == 'ic-attr-src') {
-      var value = elt.attr('ic-attr-src').split(":");
+      value = elt.attr('ic-attr-src').split(":");
       var attribute = value[0];
-      var url = value[1];
+      url = value[1];
       setIfAbsent(elt, 'ic-src', url);
       setIfAbsent(elt, 'ic-target', 'this.' + attribute);
     }
@@ -707,19 +706,23 @@ var Intercooler = Intercooler || (function () {
     if(transition == null) {
       transition = _transitions[_defaultTransition];
     }
+    if(transition == null) {
+      transition = _transitions['none'];
+    }
     return transition;
   }
 
-  function processICResponse(data, elt) {
-    if (data && /\S/.test(data)) {
-      log(elt, "IC RESPONSE: Received: " + data, "DEBUG");
+  function processICResponse(newContent, elt) {
+    if (newContent && /\S/.test(newContent)) {
+      log(elt, "IC RESPONSE: Received: " + newContent, "DEBUG");
       var target = getTarget(elt);
-      var dummy = $("<div></div>").html(data);
-      if (fp(dummy.text()) != target.attr('ic-fingerprint') || target.attr('ic-always-update') == 'true') {
+      var dummy = $("<div></div>").html(newContent);
+      processMacros(dummy);
+      if (fingerprint(dummy.html()) != target.attr('ic-fingerprint') || target.attr('ic-always-update') == 'true') {
         var transition = getTransition(elt, target);
-        transition(target, 'update', data, function () {
-          updateIntercoolerMetaData(target);
+        transition.newContent(target, newContent, false, function () {
           processNodes(target);
+          updateIntercoolerMetaData(target);
         });
       }
       dummy.remove();
@@ -830,7 +833,7 @@ var Intercooler = Intercooler || (function () {
       if (data && data['ic-setlocation']) {
         var elt = findById(data["id-to-restore"]);
         var params = getParametersForElement(elt);
-        params += "&ic-handle-pop=true"
+        params += "&ic-handle-pop=true";
         handleRemoteRequest(elt, "GET", data["restore-from"], params,
           function (data) {
             elt.trigger("handlePop.ic", elt, data);
@@ -856,10 +859,21 @@ var Intercooler = Intercooler || (function () {
     /* ===================================================
      * Core API
      * =================================================== */
-
-    refresh: function (elt) {
-      fireICRequest(elt);
+    refresh: function (val) {
+      if (typeof val == 'string' || val instanceof String) {
+        refreshDependencies(val);
+      } else {
+        fireICRequest(val);
+      }
       return Intercooler;
+    },
+
+    defaultTransition: function (name) {
+      _defaultTransition = name;
+    },
+
+    defineTransition: function (name, def) {
+      _defineTransition(name, def);
     },
 
     /* ===================================================
