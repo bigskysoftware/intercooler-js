3046bd5abd9e854c85baec681580ff935c95fd76
carson <carsongross@gmail.com>
2017-03-06 11:16:36 -0800

    Bump to 1.1.1 - fix button regression due to zepto support

--- www/release/intercooler-1.1.1.js
+++ www/release/intercooler-1.1.1.js
@@ -1,3 +1,19 @@
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module unless amdModuleId is set
+    define(["jquery"], function (a0) {
+      return (root['Intercooler'] = factory(a0));
+    });
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory(require("jquery"));
+  } else {
+    root['Intercooler'] = factory(root["jQuery"]);
+  }
+}(this, function ($) {
+
 ////////////////////////////////////
 
 /**
@@ -1789,3 +1805,7 @@ var Intercooler = Intercooler || (function() {
     }
   };
 })();
+
+return Intercooler;
+
+}));
